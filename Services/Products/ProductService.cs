5dce9c9e60dfa15a50f8a50d0ad4cfff900aa448
necmi <necmierturk@gmail.com>
2025-10-23 12:39:39 +0300

    ServiceResult

--- Services/Products/ProductService.cs
+++ Services/Products/ProductService.cs
@@ -10,9 +10,24 @@ namespace App.Services.Products
 {
     public class ProductService(IProductRepository productRepository): IProductService
     {
-        public Task<List<Product>> GetToPriceProductsAsync(int count)
+        public async Task<ServiceResult<List<Product>>> GetToPriceProductsAsync(int count)
         {
-          return productRepository.GetTopPriceProductsAsync(count);
+            var products= await productRepository.GetTopPriceProductsAsync(count)
+            return new ServiceResult<List<Product>>()
+            {
+                Data = products
+            };
+        }
+
+        public async Task<ServiceResult<Product>> GetProductByIdAsync(int id)
+        {
+            var product = await productRepository.GetByIdAsync(id);
+
+            if (product is null)
+            {
+              ServiceResult<Product>.Fail("Product not found", System.Net.HttpStatusCode.NotFound); 
+            }
+          return ServiceResult<Product>.Succecss(product!);
         }
     }
 }
---
+++ Services/ServiceResult.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Services;
+
+
+
+public class ServiceResult<T>
+{
+    public T? Data { get; set; }
+
+    public List<string>? ErrorMessage { get; set; }
+
+    public bool IsSuccess=>ErrorMessage == null || ErrorMessage.Count==0;
+
+    public bool IsFail=>!IsSuccess;
+
+    public HttpStatusCode Status {get;set;}
+
+
+    //static factory methods
+    public static ServiceResult<T> Succecss(T data,HttpStatusCode status=HttpStatusCode.OK)
+    {
+        return new ServiceResult<T>()
+        {
+            Data = data,
+            Status=status
+        };
+    }
+    public static ServiceResult<T> Fail(List<string> errors, HttpStatusCode status = HttpStatusCode.BadRequest)
+    {
+        return new ServiceResult<T>()
+        {
+            ErrorMessage = errors,
+            Status=status
+        };
+    }
+    public static ServiceResult<T> Fail(string error, HttpStatusCode status = HttpStatusCode.BadRequest)
+    {
+        return new ServiceResult<T>()
+        {//  ErrorMessage = { error}
+            ErrorMessage = new List<string>() { error }
+          
+            ,
+            Status = status
+        };
+    }
+
+
+
+}
